e79391d11d29f981a87f6594593c312c31e70e1d
revonmoore <revonmoore@gmail.com>
2025-10-13 11:25:11 -0400

    Flask assignment complete with docstrings

---
+++ app/app.py
@@ -0,0 +1,23 @@
+"""Flask application for CPS3500 assignment."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Display the default homepage message."""
+    return "Hello CPS3500!"
+
+@app.route('/new_page')
+def new_page():
+    """Display the new page message."""
+    return "This is a New Page!"
+
+@app.route('/user/<username>')
+def user(username):
+    """Render a template greeting based on the username."""
+    return render_template('user.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
